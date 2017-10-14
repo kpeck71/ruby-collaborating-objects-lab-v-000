f2c6943133d0a981eda2b44b86b5947ffc0507ac
Kristen Peck <kpeck71@gmail.com>
2017-10-14 17:38:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -36,9 +36,9 @@ class Artist
      self.new(name).tap {|artist| artist.save}
    end
 
-   def self.find_or_create_by_name(name)
-      self.find(name) ? self.find(name) : self.create(name)
-    end
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
 
   def print_songs
     @songs.collect {|s| s.name}
